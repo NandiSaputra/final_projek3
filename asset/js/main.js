eae43a8a314328aa76d549147ca39691f0f95d2f
nandi saputra <nandysyahputra59@gmail.com>
2022-12-06 12:41:34 +0700

    commit coursel

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,17 +1,29 @@
-var carouselWidth = $('.carousel-inner')[0].scrollWhidth;
-var cardWidth = $('.carousel-item').width();
+// var carouselWidth = $('.carousel-inner')[0].scrollWhidth;
+// var cardWidth = $('.carousel-item').width();
 
-var scrollPosition = 0;
+// var scrollPosition = 0;
 
-$('.carousel-control-next').on('click', function () {
- scrollPosition = scrollPosition + cardWidth;
- $('.carousel-inner').animate({scrollLeft: scrollPosition},
-  600);
-});
-$('.carousel-control-prev').on('click', function () {
- scrollPosition = scrollPosition + cardWidth;
- $('.carousel-inner').animate({scrollLeft: scrollPosition},
-  600);
- 
-
-});
+// $('.carousel-control-next').on('click', function () {
+//  scrollPosition = scrollPosition + cardWidth;
+//  $('.carousel-inner').animate({scrollLeft: scrollPosition},
+//   600);
+// });
+// $('.carousel-control-prev').on('click', function () {
+//  scrollPosition = scrollPosition + cardWidth;
+//  $('.carousel-inner').animate({scrollLeft: scrollPosition},
+//   600);
+// });
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+       
+    }
+})
